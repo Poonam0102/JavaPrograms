454c2e367b77dc10f3c3e73c2e57da34f636f92e
Sayalisangle <sayali.sangle0807@gmail.com>
2023-12-15 19:59:42 +0530

    My first commit

--- src/CoreJava/lab03.java
+++ src/CoreJava/lab03.java
@@ -3,7 +3,7 @@ package src.CoreJava;
 public class lab03 {
     public static void main(String[] args) {
 //        System.out.println("hello");
-//        System.out.print("Poonam");
+//        System.out.print("hi");
 
         
 
