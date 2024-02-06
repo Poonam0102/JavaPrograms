2db79a51b2b654506e4002c5c222f1c20b35ef71
Poonam0102 <sanglepoonam12@gmail.com>
2024-02-06 18:34:22 +0530

    Array Program

---
+++ src/Basic06/Lab109.java
@@ -0,0 +1,14 @@
+package src.Basic06;
+
+public class Lab109 {
+    public static void main(String[] args) {
+
+
+        int[] a = {1, 2, 3};
+        System.out.println(a[10]);
+
+//        ArrayIndexOutOfBoundsException
+
+    }
+
+}
